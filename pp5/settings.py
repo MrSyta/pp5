0e5c89b4532c1ca678a2fefcd15d21f1b880b478
Marcin <gamer19@poczta.fm>
2020-01-14 12:28:57 +0100

    add swagger view

--- pp5/settings.py
+++ pp5/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'pp5.app'
+    'rest_framework_swagger',
+    'pp5.app',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
+
