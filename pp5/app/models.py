6b3adcf5d973ee653ca1823ba81ea2de08068ce9
Marcin <gamer19@poczta.fm>
2020-01-22 16:57:40 +0100

    update Book model

--- pp5/app/models.py
+++ pp5/app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Book(models.Model):
-    name = models.TextField(max_length=100)
+    title = models.TextField(max_length=100)
     genre = models.TextField(max_length=50)
     price = models.FloatField()
+    author = models.TextField(max_length=50)
+    description = models.TextField(max_length=100)
+    cover = models.URLField(max_length=500)
     published = models.BooleanField(default=False)
