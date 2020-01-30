4c72f24a7014b559b89327b9d4afb594584943d0
Seven730 <michalwww3@gmail.com>
2020-01-30 12:52:41 +0100

    Added react-icons module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import Bookshop from './components/Bookshop';
-import Header from './components/layout/Header';
-import Body from './components/layout/Body';
-import './App.css';
-import axios from 'axios';
-
+import React from "react";
+import Bookshop from "./components/Bookshop";
+import Header from "./components/layout/Header";
+import Body from "./components/layout/Body";
+import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     books: [],
     cart: []
-  }
-  
+  };
+
   componentDidMount() {
-    axios.get('http://127.0.0.1:8000/books/?format=json')
-      .then(res => this.setState({ books: res.data}))
+    axios
+      .get("http://127.0.0.1:8000/books/?format=json")
+      .then(res => this.setState({ books: res.data }));
   }
-
-
 
   render() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Bookshop books={this.state.books}/>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+        <Bookshop books={this.state.books} />
+      </div>
+    );
   }
 }
 
--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,21 +1,21 @@
-import React from 'react'
-// import cart from '../../public/shopping-cart.svg'; 
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Cart() {
-    return (
-        <div style={cartStyle}>
-            <a href="../"> <img src={'../../public/shopping-cart.svg'} alt='Cart'/> </a>
-        </div>
-    )
+  return (
+    <div style={cartStyle}>
+      <a href="../">
+        {" "}
+        <FaShoppingCart />{" "}
+      </a>
+    </div>
+  );
 }
-
-function img() {
-    // Import result is the URL of your image
-    return <img src={'../../public/shopping-cart.svg'} alt="Logo" />;
-  }
 
 const cartStyle = {
-    textAlign: 'left',
-    marginLeft: '0.5rem',
-    cursor: 'pointer'
-}
+  size: "50%",
+  color: "black",
+  textAlign: "left",
+  marginLeft: "0.5rem",
+  cursor: "pointer"
+};
